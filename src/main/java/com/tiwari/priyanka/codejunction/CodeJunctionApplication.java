5343bd4396a8a4ef010ae1aece2d588d9a36e3f2
Priyanka Tiwari <priyant3@in.ibm.com>
2021-01-26 13:29:45 +0530

    initial commit

---
+++ src/main/java/com/tiwari/priyanka/codejunction/CodeJunctionApplication.java
@@ -0,0 +1,14 @@
+package com.tiwari.priyanka.codejunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeJunctionApplication {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+		SpringApplication.run(CodeJunctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiwari/priyanka/codejunction/CodeJunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiwari.priyanka.codejunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeJunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
